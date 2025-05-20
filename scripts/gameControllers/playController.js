e1c19a4a1fdcbfdc26143a9a9828e4ab38e3b581
Omnical <omnical@outlook.com>
2025-05-20 22:22:22 +1000

    start timer

--- scripts/gameControllers/playController.js
+++ scripts/gameControllers/playController.js
@@ -6,6 +6,9 @@ class PlayController {
     this.roundTimer = 30;
     this.score = 0;
 
+    this.gameStarted = false;
+    this.startTimer = 3;
+
     this.gameEnded = false;
     this.endTimer = 2;
   }
@@ -23,43 +26,60 @@ class PlayController {
     );
     pop();
 
-    if (!this.gameEnded) {
-      this.roundTimer -= dt;
-
+    // game start timer
+    if (!this.gameStarted) {
       fill(255);
       textSize(24);
-      textAlign(LEFT);
-      text("TIME: " + ceil(this.roundTimer), -width / 2 + 30, -height / 2 + 30);
-      textAlign(RIGHT);
-      text(
-        "SCORE: " + this.world.cubesActivated,
-        width / 2 - 30,
-        -height / 2 + 30
-      );
+      textAlign(CENTER);
+      text("GAME STARTING IN", 0, -20);
+      text(ceil(this.startTimer), 0, 20);
+      this.startTimer -= dt;
+      if (this.startTimer <= 0) this.gameStarted = true;
+    }
 
-      if (random() > 0.95) {
-        let randomDirection = (random() - 0.5) * 4;
-        //let randomForce = (random() - 0.5) * 4;
-        let r = (random() - 0.5) * 2;
-        let origin = r * (width / 2);
-        let direction = -r * 10 + randomDirection;
+    if (this.gameStarted) {
+      if (!this.gameEnded) {
+        this.roundTimer -= dt;
 
-        // generate cubes
-        let data = random(cubeData);
-        let cube = new Cube(data.colour, data.size, data.speed);
-        cube.position = createVector(origin, height / 2, -10);
-        //cube.acceleration = createVector(direction, (-11 + randomForce) / 1, 0); //createVector(-10, -11, 0);
-        cube.acceleration = createVector(direction, data.speed / 1, 0);
-        cube.angularVelocity = createVector(0.025, 0.025, 0.025);
-        this.world.addChild(cube);
+        fill(255);
+        textSize(24);
+        textAlign(LEFT);
+        text(
+          "TIME: " + ceil(this.roundTimer),
+          -width / 2 + 30,
+          -height / 2 + 30
+        );
+        textAlign(RIGHT);
+        text(
+          "SCORE: " + this.world.cubesActivated,
+          width / 2 - 30,
+          -height / 2 + 30
+        );
+
+        if (random() > 0.95) {
+          let randomDirection = (random() - 0.5) * 4;
+          //let randomForce = (random() - 0.5) * 4;
+          let r = (random() - 0.5) * 2;
+          let origin = r * (width / 2);
+          let direction = -r * 10 + randomDirection;
+
+          // generate cubes
+          let data = random(cubeData);
+          let cube = new Cube(data.colour, data.size, data.speed);
+          cube.position = createVector(origin, height / 2, -10);
+          //cube.acceleration = createVector(direction, (-11 + randomForce) / 1, 0); //createVector(-10, -11, 0);
+          cube.acceleration = createVector(direction, data.speed / 1, 0);
+          cube.angularVelocity = createVector(0.025, 0.025, 0.025);
+          this.world.addChild(cube);
+        }
+
+        if (this.roundTimer <= 0) {
+          this.gameEnded = true;
+        }
+      } else {
+        this.endTimer -= dt;
+        if (this.endTimer <= 0) gameStateMachine.setState(gameStates.MENU);
       }
-
-      if (this.roundTimer <= 0) {
-        this.gameEnded = true;
-      }
-    } else {
-      this.endTimer -= dt;
-      if (this.endTimer <= 0) gameStateMachine.setState(gameStates.MENU);
     }
 
     this.world.update(dt);
